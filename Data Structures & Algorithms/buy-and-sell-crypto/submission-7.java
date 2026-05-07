98246e037eed2e47d1d20ad6f074602724ed464c
jyotsnakaruturi <jyotsnakaruturi@gmail.com>
2026-05-07 13:39:40 +0530

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max =0;
+        int min = prices[0];
+        for(int i : prices){
+            min = Math.min(i,min);
+            max = Math.max(max,i-min);
+        }
+        return max;
+        
+    }
+}
