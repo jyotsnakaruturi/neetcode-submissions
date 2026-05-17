1d7aae4c829d15666849e0b35e54883a38b5ae17
jyotsnakaruturi <jyotsnakaruturi@gmail.com>
2026-05-17 09:31:48 +0530

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        l=0
+        n=len(temperatures)
+        res=[0]*(n)
+        for r in range (len(temperatures)):
+            maxe =temperatures[r]
+            l=r+1
+            while l<n-1:
+                if(maxe < temperatures[l]):
+                    maxe = temperatures[l]
+                    res[r] = l-r
+                    break
+                l+=1
+            if maxe == temperatures[r]:
+                res[r] = 0
+        return res
+
+        
