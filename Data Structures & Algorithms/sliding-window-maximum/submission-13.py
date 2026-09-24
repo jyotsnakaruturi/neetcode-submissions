e745fb975434701cd1f494e5200591433da12898
jyotsnakaruturi <jyotsnakaruturi@gmail.com>
2026-09-24 12:36:43 +0530

    Add: sliding-window-maximum - submission-13

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-13.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        j=0
+        res = []
+        maxe = float('-inf')
+        for i in range (len(nums)):
+            maxe = max(maxe,nums[i])
+            if (i-j+1) == k:
+                res.append(maxe)
+                if maxe == nums[j]:
+                    maxe = float('-inf')
+                j+=1
+        return res
+
+        
