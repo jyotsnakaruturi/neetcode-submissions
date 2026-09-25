e7239dca848cdb222f797d7411a2b83e35d7a1a6
jyotsnakaruturi <jyotsnakaruturi@gmail.com>
2026-09-25 10:35:19 +0530

    Add: validate-parentheses - submission-18

---
+++ Data Structures & Algorithms/validate-parentheses/submission-18.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        map = {'{':'}','[':']','(':')'} 
+        seen = []
+        for i in s:
+            if i == '{' or i == '(' or i == '[':
+                seen.append(i)
+            else:
+                if i != map[seen[-1]]:
+                    return False
+                else:
+                    seen.pop()
+        if seen:
+            return False
+        else:
+            return True
